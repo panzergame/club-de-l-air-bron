c605196f0e4bca7da4a5ee203836f173eee8b948
tristan <tristan.porteries@gmail.com>
2024-10-16 21:56:02 +0200

    feat: ajout adress club

--- src/pages/reunions.tsx
+++ src/pages/reunions.tsx
@@ -5,8 +5,9 @@ import Layout from "../components/layout"
 const Page: React.FC<PageProps> = () => {
   return (
     <Layout>
-      <h2>Calendrier des réunions</h2>
-      <iframe className="w-100" height="800"
+      <h2>Réunions</h2>
+      Le club se retrouve régulièrement un samedi sur deux au <a href="geo:45.7473954,4.9148733">18 rue de la pagère 69500 Bron</a>
+      <iframe className="w-100 mt-5" height="800"
         src="https://calendar.google.com/calendar/embed?src=1379a8a62044fc804b336b12c9a7fbd8aca9cf71f87dc63eddae06092dea75e4%40group.calendar.google.com&ctz=Europe%2FParis"></iframe>
     </Layout>
   )
